7fa5bf64a24051b765be7585119a05573a1ab87d
briand3 <briand@arris.com.au>
2025-03-25 16:29:15 +1030

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+set -e
 
 cd /var/www/laravel-demo
 
-# Ensure correct permissions
+# Set correct permissions
 chown -R www-data:www-data .
 
-# Laravel setup
+# Fetch .env from SSM Parameter Store
+PARAM="laravel-demo-env"
+REGION="ap-southeast-2"
+
+aws ssm get-parameter \
+  --with-decryption \
+  --name "$PARAM" \
+  --region "$REGION" \
+  --query Parameter.Value \
+  --output text > /var/www/laravel-demo/.env
+
+# Install dependencies
 composer install --no-dev --optimize-autoloader
+
+# Laravel setup
+php artisan key:generate
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
